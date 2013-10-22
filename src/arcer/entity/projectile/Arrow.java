4779d8a4b36202188caec2a6aef5098ec88a9fea
Shiliang Zhang <lsftw13@gmail.com>
2013-10-21 22:19:22 -0400

    Added arcing to arrows.

--- src/arcer/entity/projectile/Arrow.java
+++ src/arcer/entity/projectile/Arrow.java
@@ -3,19 +3,19 @@ package arcer.entity.projectile;
 import arcer.entity.Entity;
 import arcer.resource.Settings;
 
+// TODO angle of fire
 public class Arrow extends Projectile {
 	public Arrow(Entity owner, float xpos, float ypos) {
 		super(owner, xpos, ypos);
 		damage = 25;
-		targetting = TargetType.ENEMY;
-		// TODO arcs like a parabola
-		vx = owner.isFacingRight() ? 5 : -5;
-		vy = 10;
+		targetting = TargetType.ALL;
+		vx = owner.isFacingRight() ? 2 : -2;
+		vy = -10;
 
-		duration = Settings.valueInt("fps");
+		duration = Settings.valueInt("fps") * 10;
 		terrainCollidable = false;
 	}
 	public void preDt() {
-		vy -= .05; // gravity
+		vy += 5f / (Settings.valueInt("fps")); // gravity
 	}
 }
