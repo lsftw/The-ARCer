7bebc463b36757d318f612039cc94515c5d91d0a
Shiliang Zhang <lsftw13@gmail.com>
2013-10-29 18:38:36 -0400

    Added shield.

---
+++ src/arcer/entity/player/Shield.java
@@ -0,0 +1,10 @@
+package arcer.entity.player;
+
+import arcer.core.Zone;
+import arcer.entity.Entity;
+
+public class Shield extends Entity {
+	public Shield(Zone container, float xpos, float ypos) {
+		super(container, xpos, ypos);
+	}
+}
